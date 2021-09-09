27f26ab365383fd53efc2841f35cfbddc63adb4f
Matthew Thornington <abj@qlik.com>
2021-09-09 13:39:40 +0200

    change

--- hello.go
+++ hello.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
-	var x int = 1
-	var ip *int
-	ip = &x
-	fmt.Println(*ip)
+	// var x int = 1
+	// var ip *int
+	// ip = &x
+	// fmt.Println(*ip)
 }
